1c53575ce147554159930db98aa55b1a8747d5a4
Prosper Coded <enweremproper@gmail.com>
2025-09-29 21:06:53 +0100

    Integrate ConfigModule and LoggerModule into API and Background modules for enhanced configuration and logging capabilities.

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
+import { ConfigModule, LoggerModule } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule, LoggerModule],
   controllers: [ApiController],
   providers: [ApiService],
 })
--- apps/background/src/background.module.ts
+++ apps/background/src/background.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BackgroundController } from './background.controller';
 import { BackgroundService } from './background.service';
+import { ConfigModule, LoggerModule } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule, LoggerModule],
   controllers: [BackgroundController],
   providers: [BackgroundService],
 })
