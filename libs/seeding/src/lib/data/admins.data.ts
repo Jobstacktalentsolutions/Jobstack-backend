7be54fb3c680e148c8c8723484d0a59769008664
Prosper Coded <enweremproper@gmail.com>
2026-01-23 10:15:45 +0100

    feat: Update super admin password handling and environment variable validation
    - Added dotenv configuration to load environment variables.
    - Changed password handling to hash the super admin password before returning it.
    - Enhanced error logging for missing environment variables in super admin configuration.

--- libs/seeding/src/lib/data/admins.data.ts
+++ libs/seeding/src/lib/data/admins.data.ts
@@ -1,22 +1,25 @@
 import { AdminRole } from '@app/common/shared/enums/roles.enum';
 import * as bcrypt from 'bcryptjs';
 import { CONSTANT_IDS } from './constant.data';
-
+import dotenv from 'dotenv';
+dotenv.config();
 /**
  * Get super admin configuration from environment variables with fallbacks
  */
 function getSuperAdminConfig() {
   const email = process.env.SUPER_ADMIN_EMAIL!;
-  const password = bcrypt.hashSync(process.env.SUPER_ADMIN_PASSWORD!, 12);
+  const password = process.env.SUPER_ADMIN_PASSWORD!;
   const firstName = process.env.SUPER_ADMIN_FIRST_NAME!;
   const lastName = process.env.SUPER_ADMIN_LAST_NAME!;
 
   if (!email || !password || !firstName || !lastName) {
+    console.error({ email, password, firstName, lastName });
     throw new Error('Super admin configuration is missing');
   }
+  const passwordHash = bcrypt.hashSync(password, 12);
   return {
     email,
-    password,
+    password: passwordHash,
     firstName,
     lastName,
   };
